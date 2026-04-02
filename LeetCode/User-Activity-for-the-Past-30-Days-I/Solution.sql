e10f4c14fb93a0d5b4890d83396d050165d248b7
Utkarsh-Singh-30 <144414649+Utkarsh-Singh-30@users.noreply.github.com>
2026-04-02 23:47:40 +0530

    Add solution for User Activity for the Past 30 Days I

--- LeetCode/User-Activity-for-the-Past-30-Days-I/Solution.sql
+++ LeetCode/User-Activity-for-the-Past-30-Days-I/Solution.sql
@@ -1,8 +1,1 @@
-1# Write your MySQL query statement below
-2
-3SELECT 
-4    activity_date AS day, 
-5    COUNT(DISTINCT user_id) AS active_users
-6FROM Activity
-7WHERE activity_date > '2019-06-27' AND activity_date <= '2019-07-27'
-8GROUP BY activity_date;
+activity_date > '2019-06-27' AND activity_date <= '2019-07-27' 
