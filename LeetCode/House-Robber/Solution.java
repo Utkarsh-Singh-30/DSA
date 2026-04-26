e40e968cf43a39d68a83c7776e08b0bd77fcb3d9
Utkarsh-Singh-30 <144414649+Utkarsh-Singh-30@users.noreply.github.com>
2026-04-26 16:55:27 +0530

    Add solution for House Robber

--- LeetCode/House-Robber/Solution.java
+++ LeetCode/House-Robber/Solution.java
@@ -1,24 +1,49 @@
 1class Solution {
 2    public int rob(int[] nums) {
-3        int n= nums.length;
+3        int n =nums.length;
 4        if(n==1) return nums[0];
-5        if(n==2) return Math.max(nums[0], nums[1]);
-6        // int []dp = new int[n];
-7        // dp[0]=nums[0];
-8        // dp[1]=Math.max(nums[0],nums[1]);
-9        // for(int i=2;i<n;i++){
-10        //     dp[i]=Math.max(dp[i-1], nums[i]+dp[i-2]);
-11        // }
-12
-13        // return dp[n-1];
-14        int i=nums[0];
-15        int j=Math.max(nums[0], nums[1]);
-16        int ans=0;
-17        for(int k=2;k<n;k++){
-18            ans=Math.max(j,nums[k]+i);
-19            i=j;
-20            j=ans;
-21        }
-22        return ans;
-23    }
-24}
+5
+6        // return helper(0,nums, n);
+7
+8        // int []memo= new int [n];
+9        // Arrays.fill(memo,-1);
+10        // return helper2(0,nums, n,memo);
+11
+12        return helper3(nums,n);
+13    }
+14
+15
+16    public int helper(int i, int []nums, int n){
+17
+18        if(i>=n) return 0;
+19
+20        return Math.max(nums[i] + helper(i+2,nums, n), helper(i+1,nums, n));
+21    }
+22
+23    public int helper2(int i, int []nums, int n, int []memo){
+24
+25        if(i>=n) return 0;
+26
+27        if(memo[i]!=-1) return memo[i];
+28
+29        memo[i]= Math.max(nums[i] + helper2(i+2,nums, n,memo), helper2(i+1,nums, n,memo));
+30        return memo[i];
+31    }
+32
+33    public int helper3(int []nums, int n ){
+34        int []dp = new int [n];
+35        
+36        if(n==2) return Math.max(nums[0], nums[1]);
+37
+38        dp[n-1]=nums[n-1];
+39        dp[n-2]= Math.max(nums[n-1], nums[n-2]);
+40
+41        for(int i=n-3;i>=0;i--){
+42            dp[i]=Math.max(nums[i]+dp[i+2], dp[i+1]);
+43        }
+44
+45        return dp[0];
+46    }
+47
+48
+49}
