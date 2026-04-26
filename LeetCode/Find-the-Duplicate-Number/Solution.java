d5ed34fc764918b10ffa87d4ada5e57d1bd93afa
Utkarsh-Singh-30 <144414649+Utkarsh-Singh-30@users.noreply.github.com>
2026-04-26 12:10:15 +0530

    Add solution for Find the Duplicate Number

--- LeetCode/Find-the-Duplicate-Number/Solution.java
+++ LeetCode/Find-the-Duplicate-Number/Solution.java
@@ -1,18 +1,24 @@
 1class Solution {
-2    public int findDuplicate(int[] nums) {
-3        int slow= nums[0];
-4        int fast= nums[0];
-5
-6        do{
-7            slow=nums[slow];
-8            fast=nums[nums[fast]];
-9        }while(slow!=fast);
-10
-11        slow=nums[0];
-12        while(slow!=fast){
-13            slow=nums[slow];
-14            fast=nums[fast];
-15        }
-16        return slow;
-17    }
-18}
+2    public static int findDuplicate(int[] nums) {
+3        int len = nums.length;
+4        int low = 1;
+5        int high = len - 1;
+6        while (low < high) {
+7            int mid = low + (high - low) / 2;
+8            int cnt = 0;
+9            for (int i = 0; i < len; i++) {
+10                if (nums[i] <= mid) {
+11                    cnt++;
+12                }
+13            }
+14
+15            if (cnt <= mid) {
+16                low = mid + 1;
+17            } else {
+18                high = mid;
+19            }
+20        }
+21
+22        return low;
+23    }
+24}
