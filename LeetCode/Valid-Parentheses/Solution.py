302fb9657d8adc32b095eec6bf7f445086ae7316
Utkarsh-Singh-30 <144414649+Utkarsh-Singh-30@users.noreply.github.com>
2026-05-08 11:37:11 +0530

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.py
+++ LeetCode/Valid-Parentheses/Solution.py
@@ -1,18 +1,21 @@
-1class Solution {
-2    public boolean isValid(String s) {
-3       Stack<Character> st = new Stack<>();
-4        for(int i =0;i<s.length();i++){
-5            if(s.charAt(i)=='(' || s.charAt(i)=='{' ||s.charAt(i)=='[')
-6                st.push(s.charAt(i));
-7            else{
-8                if(st.isEmpty()) return false;
-9                else if(s.charAt(i)==')'&& st.peek()=='(') st.pop();
-10                else if(s.charAt(i)=='}'&& st.peek()=='{') st.pop();
-11                else if(s.charAt(i)==']'&& st.peek()=='[') st.pop();
-12                else return false;
-13            }
-14        }
-15        if(st.isEmpty()) return true;
-16        else return false;
-17    }
-18}
+class Solution:
+    def isValid(self, s: str) -> bool:
+        i=0
+        a=[]
+        for i in range(len(s)):
+            if s[i]=='('or s[i]=='['or s[i]=='{':
+                a.append(s[i])
+            else:
+                if not a:
+                    return False
+                top=a.pop()
+                if s[i]==')'and top!='(':
+                    return False
+                if s[i]==']'and top!='[':
+                    return False
+                if s[i]=='}'and top!='{':
+                    return False
+        return len(a)==0
+
+            
+        
