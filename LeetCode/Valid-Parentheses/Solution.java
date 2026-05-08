b260c4010309cee1899ade9432e3cfbd14d1a9ae
Utkarsh-Singh-30 <144414649+Utkarsh-Singh-30@users.noreply.github.com>
2026-05-08 11:17:48 +0530

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.java
+++ LeetCode/Valid-Parentheses/Solution.java
@@ -1,15 +1,18 @@
-1class Solution:
-2    def isValid(self, s: str) -> bool:
-3        stack=[]
-4        for i in range(0,len(s)):
-5            if s[i]=='(' or s[i]=='{' or s[i]=='[':
-6                stack.append(s[i])
-7            else:
-8                if len(stack)==0:
-9                    return False
-10                elif s[i]==')' and stack[-1]=='(': stack.pop()
-11                elif s[i]=='}' and stack[-1]=='{': stack.pop()
-12                elif s[i]==']' and stack[-1]=='[': stack.pop()
-13                else: return False
-14        if len(stack)==0:
-15            return True
+1class Solution {
+2    public boolean isValid(String s) {
+3       Stack<Character> st = new Stack<>();
+4        for(int i =0;i<s.length();i++){
+5            if(s.charAt(i)=='(' || s.charAt(i)=='{' ||s.charAt(i)=='[')
+6                st.push(s.charAt(i));
+7            else{
+8                if(st.isEmpty()) return false;
+9                else if(s.charAt(i)==')'&& st.peek()=='(') st.pop();
+10                else if(s.charAt(i)=='}'&& st.peek()=='{') st.pop();
+11                else if(s.charAt(i)==']'&& st.peek()=='[') st.pop();
+12                else return false;
+13            }
+14        }
+15        if(st.isEmpty()) return true;
+16        else return false;
+17    }
+18}
