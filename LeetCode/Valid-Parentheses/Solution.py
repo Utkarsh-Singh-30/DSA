f348cf9fade9a4c485e80c358e3e40651ab97481
Utkarsh-Singh-30 <144414649+Utkarsh-Singh-30@users.noreply.github.com>
2026-05-08 11:39:09 +0530

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.py
+++ LeetCode/Valid-Parentheses/Solution.py
@@ -1,21 +1,19 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        i=0
-        a=[]
-        for i in range(len(s)):
-            if s[i]=='('or s[i]=='['or s[i]=='{':
-                a.append(s[i])
-            else:
-                if not a:
-                    return False
-                top=a.pop()
-                if s[i]==')'and top!='(':
-                    return False
-                if s[i]==']'and top!='[':
-                    return False
-                if s[i]=='}'and top!='{':
-                    return False
-        return len(a)==0
-
-            
-        
+1class Solution:
+2    def isValid(self, s: str) -> bool:
+3        n= len(s)
+4        st= []
+5
+6        for i in range(n):
+7            if s[i] in {'(','{','['}:
+8                st.append(s[i])
+9
+10            else:
+11                if len(st) == 0: return False 
+12                elif(s[i]==')' and st[len(st)-1])=='(': st.pop()
+13                elif(s[i]=='}' and st[len(st)-1])=='{': st.pop()
+14                elif(s[i]==']' and st[len(st)-1])=='[': st.pop()
+15                else: return False
+16
+17        if len(st) == 0: return True
+18        else: return False
+19
