a06c7da955f7f2ccd20cabe9affe840cae0b3e78
Utkarsh-Singh-30 <144414649+Utkarsh-Singh-30@users.noreply.github.com>
2026-07-29 20:20:36 +0530

    Add solution for Best Time to Buy and Sell Stock

--- LeetCode/Best-Time-to-Buy-and-Sell-Stock/Solution.py
+++ LeetCode/Best-Time-to-Buy-and-Sell-Stock/Solution.py
@@ -1,13 +1,14 @@
 1class Solution {
 2    public int maxProfit(int[] prices) {
-3        int max_profit=0,min =prices[0];
-4        for(int i=0;i<prices.length;i++){
-5            if(min>prices[i])
-6            min=prices[i];
-7            int profit=prices[i]-min;
-8            if(max_profit<profit)
-9            max_profit=profit;
-10        }
-11        return max_profit;
-12    }
-13}
+3        int buy=prices[0];
+4        int max=0;
+5
+6        for(int i=1;i<prices.length;i++){
+7            if(prices[i]<buy) buy=prices[i];
+8
+9            int profit=prices[i]-buy;
+10            max=Math.max(profit,max);
+11        }
+12        return max;
+13    }
+14}
